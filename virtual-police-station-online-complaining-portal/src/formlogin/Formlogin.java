3d4ac48cbfb396048a539a8106a7f99cc616a221
Ruthsan R <ruthruthsan@gmail.com>
2020-08-12 23:06:28 +0530

    added all files

---
+++ virtual-police-station-online-complaining-portal/src/formlogin/Formlogin.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formlogin;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author ROSE
+ */
+public class Formlogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     NewJFrame AI = new NewJFrame();
+     AI.setVisible(true);
+     AI.setBackground(new Color(0,0,0,0));
+    }
+    
+}
